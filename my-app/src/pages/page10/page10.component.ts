f6b69c0a1430f94faf7a3d5169cd1224c3a6925c
longtenglin <nanzhaolove@gmail.com>
2024-09-24 17:59:09 +0900

    S3文件上传 1. 创建预签名URL 2. 使用预签名URL上传文件

--- my-app/src/pages/page10/page10.component.ts
+++ my-app/src/pages/page10/page10.component.ts
@@ -2,8 +2,9 @@ import {Component, HostBinding} from "@angular/core";
 import {API_LIST_TYPE} from "../../app/app.type";
 import {NgForOf, NgIf} from "@angular/common";
 import { MatSelect } from "@angular/material/select";
-import { CreateBucketCommand, S3Client } from "@aws-sdk/client-s3";
+import {CreateBucketCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import {getSignedUrl} from "@aws-sdk/s3-request-presigner";
 
 @Component({
   selector: 'app-page-10'
@@ -14,7 +15,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 
 export class Page10Component {
-  
+
   constructor(private httpClient: HttpClient) {
   }
 
@@ -22,29 +23,37 @@ export class Page10Component {
   classes: string = 'app-page-flex app-page-flex-1'
 
   apiList: API_LIST_TYPE[] = API_LIST
-  
-  client = new S3Client({
-    region: "ap-northeast-1"
-  });
-  
-  
-  createBucket  = async () => {
-    // const command = new CreateBucketCommand({
-    //   Bucket: "angular_test_create"
-    // })
-    //
-    // try {
-    //   const { Location } = await this.client.send(command);
-    //   console.log(`Bucket created with location ${Location}`)
-    // } catch (err) {
-    //   console.error(err)
-    // }
-    const params = {
-      url: "http://localhost:8080",
-      data: "[2024/08/16_17:14:18.990] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:30:22.134] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:36:40.633] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:37:57.667] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:38:21.292] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:40:26.322] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:40:40.777] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:44:07.563] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:44:16.063] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_17:51:20.127] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:11:12.239] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:12:02.034] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:14:44.614] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:17:44.393] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:27:02.028] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:29:29.525] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:30:05.506] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:34:07.803] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:39:32.043] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n[2024/08/16_18:40:17.512] ERROR SdeExecSql - E:execsql #Failed to SQL. :bindParamキーの指定がありません。\\r\\n"
+
+  /**
+   * 创建预签名URL用于上传文件
+   * @param region
+   * @param bucket
+   * @param key
+   */
+  createPresignedUrlWithClient = ({ region, bucket, key }: any) => {
+    const client = new S3Client({
+      region,
+      credentials: {accessKeyId: "AKIAX3DNHYXXXXXXXXX", secretAccessKey: "AMfu+HFyXNZXXXXXXXXXXXXXXXXXXXXXXXXXXX"}
+    });
+    const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+    return getSignedUrl(client, command, { expiresIn: 3600 });
+  }
+
+
+  uploadFiles  = async () => {
+    let params = {
+      url: "",
+      data: "文件内容，以字符串的形式上传，云端自动创建 logs3.txt 文件，然后写入内容"
     }
+
+    params.url = await this.createPresignedUrlWithClient({
+      region: "us-east-1",
+      bucket: "ap-test-logs-2",
+      key: "logs3.txt"
+    })
+
     this.httpClient.put(params.url, params.data, {
-      headers: new HttpHeaders({"Content-Length": new Blob([params.data]).size, "Access-Control-Allow-Origin": "*"})
+      headers: new HttpHeaders({'Content-Type': 'text/plain; charset=UTF-8'}),
     }).subscribe(response => {
       console.log(response)
     })
